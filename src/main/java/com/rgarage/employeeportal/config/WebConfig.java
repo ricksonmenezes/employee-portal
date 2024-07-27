fec66b236f162ac1b6efde41fd051cca5045347e
ricksonmenezes <ricksonmenezes@gmail.com>
2024-07-27 16:16:42 +0800

    registerd web-inf

---
+++ src/main/java/com/rgarage/employeeportal/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.rgarage.employeeportal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
