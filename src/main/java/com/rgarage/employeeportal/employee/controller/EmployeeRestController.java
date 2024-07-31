d9792e6d147962aaaddc30480d4a0c6b383bb7b2
ricksonmenezes <ricksonmenezes@gmail.com>
2024-07-31 10:20:43 +0800

    added graphql endpoint. will explore it later without using spqr library

--- src/main/java/com/rgarage/employeeportal/employee/controller/EmployeeRestController.java
+++ src/main/java/com/rgarage/employeeportal/employee/controller/EmployeeRestController.java
@@ -1,22 +1,43 @@
 package com.rgarage.employeeportal.employee.controller;
 
-import com.rgarage.employeeportal.employee.domain.Employee;
+import com.rgarage.employeeportal.employee.domain.TestEmployeeResponse;
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.GraphQLException;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class EmployeeRestController {
 
-    @GetMapping("/employees")
-    public Employee[] getEmployees() {
+    @Autowired
+    private  GraphQL graphQL;
 
-        Employee[] employees = new Employee[3];
+    @GetMapping("/employees")
+    public TestEmployeeResponse[] getEmployees() {
+
+        TestEmployeeResponse[] employees = new TestEmployeeResponse[3];
 
         // Create Employee instances and assign them to the array
-        employees[0] = new Employee("E001", "Alice", "Software Engineer");
-        employees[1] = new Employee("E002", "Bob", "Project Manager");
-        employees[2] = new Employee("E003", "Charlie", "QA Tester");
+        employees[0] = new TestEmployeeResponse("E001", "Alice", "Software Engineer");
+        employees[1] = new TestEmployeeResponse("E002", "Bob", "Project Manager");
+        employees[2] = new TestEmployeeResponse("E003", "Charlie", "QA Tester");
         return employees;
 
     }
+
+    @PostMapping(value = "/graphql")
+    public Map<String, Object> execute(@RequestBody Map<String, String> request, HttpServletRequest raw)
+            throws GraphQLException {
+        ExecutionResult result = graphQL.execute(request.get("query"));
+
+        return result.getData();
+    }
 }
+
