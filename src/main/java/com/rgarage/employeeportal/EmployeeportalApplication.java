c0c7f0c2ec4adc85bbe029381e2f1b0bfc05ed68
ricksonmenezes <ricksonmenezes@gmail.com>
2024-07-31 10:20:02 +0800

    these settings to set my system to utc did not work. will remove it later

--- src/main/java/com/rgarage/employeeportal/EmployeeportalApplication.java
+++ src/main/java/com/rgarage/employeeportal/EmployeeportalApplication.java
@@ -3,11 +3,22 @@ package com.rgarage.employeeportal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class EmployeeportalApplication {
 
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
 	public static void main(String[] args) {
+		System.setProperty("user.timezone", "UTC");
 		SpringApplication.run(EmployeeportalApplication.class, args);
+
+
 	}
 
 
